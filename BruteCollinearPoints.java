328806ef574a68393f567c0dbbcafa5c2b6a5568
Kaleab <mekonenkaleab0@gmail.com>
2019-04-05 15:02:49 +0300

    Final commit

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,53 +1,71 @@
+/**
+ *
+ * @Author Kaleab Mekonen
+ *
+ * */
+
 import java.util.Arrays;
-import java.util.Vector;
 
 public class BruteCollinearPoints {
-    private final Point[] points;
-    private  int numberOfSegments;
+
+    private int num_segments = 0;
+    private LineSegment[] found_segments;
+
     public BruteCollinearPoints(Point[] points) {
-        int numberOfPoints = points.length;
-        if(numberOfPoints == 0) throw new IllegalArgumentException("NO POINTS");
-        for(int i = 0; i < numberOfPoints; i++) {
-            if(points[i] == null) throw new IllegalArgumentException("NULL POINT DETECTED");
-        }
-        Arrays.sort(points); // since it implements the comparable interface and overrides the compareTo method;
-        // slope from the initial point to the next , the next to the next to the next;
-        for(int i = 0; i < numberOfPoints - 1; i++){
-            if(points[i].slopeTo(points[i+1]) == Double.NEGATIVE_INFINITY)
-                throw new IllegalArgumentException("REPEATED POINT IS DETECTED");
-        }
+        if (points == null) throw new java.lang.IllegalArgumentException("The input argument is null");
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) throw new java.lang.IllegalArgumentException("The input contains null points");
 
-        // If there is no repeated line, null points, and if the array is not empty, then sort the array and
-        // assign the property
-
-        this.points = points.clone();
-
-    }
-
-    public int numberOfSegments() {
-        return this.numberOfSegments;
-    }
-
-    public LineSegment[] segments() {
-        Vector<LineSegment> lineSegments = new Vector();
-
-        Arrays.sort(points);
-        double prevSlope = Double.NEGATIVE_INFINITY;
-
-        int numberOfPoints = points.length;
-        for(int i = 0; i < numberOfPoints - 1; i++) {
-            for(int j = i + 1; j < numberOfPoints - 1; j++) {
-                double newSlope = points[i].slopeTo(points[j]);
-                if(newSlope != prevSlope){
-                    numberOfSegments++;
-                    lineSegments.add(new LineSegment(points[i], points[j]));
-                    prevSlope = newSlope;
-                }
+            for (int j = i+1; j < points.length; j++) {
+                if (points[j] == null) throw new java.lang.IllegalArgumentException("The input contains null points");
+                if (points[i].compareTo(points[j]) == 0) throw new java.lang.IllegalArgumentException("The input contains repeated points");
             }
         }
 
-        LineSegment[] array = new LineSegment[lineSegments.size()];
-        return lineSegments.toArray(array);
+        num_segments = 0;
+        found_segments = new LineSegment[points.length];
+
+        for (int i = 0; i < points.length; i++)
+            for (int j = i+1; j < points.length; j++)
+                for (int k = j+1; k < points.length; k++) {
+
+                    // Checking if the first 3 points are collinear
+                    Point p = points[i];
+                    Point q = points[j];
+                    Point r = points[k];
+
+                    if (p.slopeTo(q) == p.slopeTo(r))
+
+                        // If they are collinear, we look for a forth one
+                        for (int l = k+1; l < points.length; l++) {
+                            Point s = points[l];
+                            if (p.slopeTo(s) == p.slopeTo(q)) {
+                                addSegment(p, q, r, s);
+                            }
+                        }
+
+                }
 
     }
+    public int numberOfSegments() { return num_segments; }
+    public LineSegment[] segments() {
+        return Arrays.copyOfRange(found_segments, 0, numberOfSegments());
+    }
+
+    private void addSegment(Point p, Point q, Point r, Point s) {
+
+        // Finding the most separated set of points
+        Point[] collinear_points = {p, q, r, s};
+        Arrays.sort(collinear_points);
+        Point min_point = collinear_points[0];
+        Point max_point = collinear_points[3];
+
+        found_segments[num_segments++] = new LineSegment(min_point, max_point);
+    }
+
+    public static void main(String[] args) {
+        // Empty method
+    }
+
+
 }
--- Point.java
+++ Point.java
@@ -1,62 +1,82 @@
-import edu.princeton.cs.algs4.StdDraw;
+/**
+ *
+ * @Author Kaleab Mekonen
+ *
+ * */
 
 import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class Point implements Comparable<Point> {
-    private final int x;
-    private final int y;
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
 
     public Point(int x, int y) {
+        /* DO NOT MODIFY */
         this.x = x;
         this.y = y;
     }
 
+
     public void draw() {
-        StdDraw.point(this.x, this.y);
+
+        StdDraw.point(x, y);
     }
 
+
     public void drawTo(Point that) {
+        /* DO NOT MODIFY */
         StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
     public double slopeTo(Point that) {
-        int changeInY = this.y - that.y;
-        int changeInX = this.x - that.x;
-        if (changeInX == 0 && changeInY == 0) {
-            return Double.NEGATIVE_INFINITY;
-        } else if (changeInY == 0) {
-            return Double.POSITIVE_INFINITY;
+        if (this.x == that.x)
+            if (this.y == that.y) return Double.NEGATIVE_INFINITY;  // Both points are the same
+            else return Double.POSITIVE_INFINITY;  // Vertical line
+
+        if (this.y == that.y)
+            return 0.0;  // Horizontal line
+
+        else return ((double) (that.y - this.y) / (double) (that.x - this.x));
+    }
+
+    public int compareTo(Point that) {
+        int difY = this.y - that.y;
+        if (difY != 0){
+            return difY;
         } else {
-            return (double) (changeInY) / (double) (changeInX);
+            return (this.x - that.x);
         }
     }
 
+    public Comparator<Point> slopeOrder() {
+        return new BySlope(this);
+    }
+
+    private static class BySlope implements Comparator<Point> {
+        private final Point p;
+
+        public BySlope(Point p) {
+            this.p = p;
+        }
+
+        public int compare(Point p1, Point p2) {
+            double slopeTo1 = this.p.slopeTo(p1);
+            double slopeTo2 = this.p.slopeTo(p2);
+
+            if (slopeTo1 > slopeTo2) return 1;
+            if (slopeTo1 < slopeTo2) return -1;
+            else return 0;
+        }
+    }
 
     public String toString() {
+
         return "(" + x + ", " + y + ")";
     }
 
-    @Override
-    public int compareTo(Point that) {
-        if (this.y != that.y) {
-            return this.y - that.y;
-        } else {
-            return this.x - that.x;
-        }
-    }
-
-
-    public Comparator<Point> slopeOrder() {
-
-        return new Comparator<Point>() {
-            @Override
-            public int compare(Point o1, Point o2) {
-                double difference = slopeTo(o1) - slopeTo(o2);
-                if (difference < 0) return -1;
-                else if (difference > 0) return 1;
-                else return 0;
-            }
-        };
-
+    public static void main(String[] args) {
+        // Empty method
     }
 }
